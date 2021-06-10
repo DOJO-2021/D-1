8860f27d60a07684e422caf0ecd1e46975d458fb
dojo2021-S-yamada <yamada@seplus2016.onmicrosoft.com>
2021-06-10 10:27:56 +0900

    名前大文字変更

--- SEEGGS/src/servlet/aHomeServlet.java
+++ SEEGGS/src/servlet/aHomeServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class aHomeServlet
  */
-@WebServlet("/aHomeServlet")
+@WebServlet("/AHomeServlet")
 public class aHomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
