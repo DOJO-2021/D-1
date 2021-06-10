fdd2355e3c7784f39141153ec62b4c2057fed58b
dojo2021-S-yamada <yamada@seplus2016.onmicrosoft.com>
2021-06-10 10:37:01 +0900

    WEB-INF,src小文字aをAに変更

--- SEEGGS/src/servlet/aHomeServlet.java
+++ SEEGGS/src/servlet/aHomeServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,13 +12,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class aHomeServlet
  */
 @WebServlet("/AHomeServlet")
-public class aHomeServlet extends HttpServlet {
+public class AHomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public aHomeServlet() {
+    public AHomeServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
