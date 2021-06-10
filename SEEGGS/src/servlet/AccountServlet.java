fda5c31ce4fb9a7375df3ebd593e0103e840304a
dojo2021-majima <mazima@seplus2016.onmicrosoft.com>
2021-06-10 14:38:43 +0900

    変更

--- SEEGGS/src/servlet/AccountServlet.java
+++ SEEGGS/src/servlet/AccountServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDAO;
 
 /**
  * Servlet implementation class AccountServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/AccountServlet")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +31,54 @@ public class AccountServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+		// 登録ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Account.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("id") == null) {
+					response.sendRedirect("/SEEGGS/LoginServlet");
+					return;
+				}
+				// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String name = request.getParameter("NAME");
+				String nickname = request.getParameter("NICKNAME");
+				String company = request.getParameter("COMPANY");
+				String hobby = request.getParameter("HOBBY");
+				String birthplace = request.getParameter("BIRTHPLACE");
+				String thisisme = request.getParameter("THISISME");
+				String future = request.getParameter("FUTURE");
+				String word = request.getParameter("WORD");
+
+				// 画像のアップロード方法について要確認
+
+				// 登録処理を行う
+				UserDAO uDao = new UserDAO();
+				if (uDao.insert(new Account(0, name, nickname, company, hobby, birthplace, thisisme, future, word))) {	// 登録成功
+					request.setAttribute("result",
+					new Result("登録完了", "/SEEGGS/HomeServlet"));
+				}
+				else {												// 登録失敗
+					request.setAttribute("result",
+					new Result("登録を正常に行うことができませんでした", "/SEEGS/HomeServlet"));
+				}
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Result.jsp");
+				dispatcher.forward(request, response);
 	}
 
 }
--- SEEGGS/src/servlet/UserServlet.java
+++ SEEGGS/src/servlet/UserServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDAO;
 
 /**
  * Servlet implementation class UserServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +31,51 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+		// ユーザページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/User.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/SEEGGS/LoginServlet");
+				return;
+			}
+
+			// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String photo = request.getParameter("PHOTO");
+			String name = request.getParameter("NAME");
+			String company = request.getParameter("COMPANY");
+			String nickname = request.getParameter("NICKNAME");
+			String birthplace = request.getParameter("BIRTHPLACE");
+			String thisisme = request.getParameter("THISISME");
+			String hobby = request.getParameter("HOBBY");
+			String future = request.getParameter("FUTURE");
+			String word = request.getParameter("WORD");
+
+			// 検索処理を行う
+			UserDAO uDao = new UserDAO();
+			List<Personal> cardList = uDao.select(new Personal(0, photo, name, company, nickname, birthplace, thisisme, hobby, future, word));
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("cardList", cardList);
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/User.jsp");
+			dispatcher.forward(request, response);
 	}
+
 
 }
