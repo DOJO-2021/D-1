233bc471356841c1c1ad4a7f852372dcb7a30398
dojo2021-k-toyota <toyota@seplus2016.onmicrosoft.com>
2021-06-10 14:20:18 +0900

    6/10更新

--- SEEGGS/src/dao/LoginDAO.java
+++ SEEGGS/src/dao/LoginDAO.java
@@ -1,5 +1,60 @@
 package dao;
 
-public class LoginDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+//ログインの判定
+public class LoginDAO {
+	public boolean isLoginOK(String id, String password) {
+		Connection conn = null;
+		boolean loginResult = false;
+
+			try {
+			//JDBCドライブを読み込む
+			Class.forName("org.h2.Driver");
+
+			//データベースへ接続
+			//要修正
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-1/SEEGGS", "sa", "");
+
+			//select文を準備する
+			String sql = "select count(*) from login where ID = ? and PASSWORD = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, id);
+			pStmt.setString(2, password);
+
+			//SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			//ユーザidとパスワードが一致するユーザがいたかどうかチェックする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		finally {
+			//データベースを切断
+			if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+			}
+		}
+		}
+			return loginResult;
+		}
 }
--- SEEGGS/src/model/LoginBeans.java
+++ SEEGGS/src/model/LoginBeans.java
@@ -1,5 +1,37 @@
 package model;
+import java.io.Serializable;
 
-public class LoginBeans {
+public class LoginBeans implements Serializable{
+//idとpwのフィールドの生成
+	private String id;
+	private String pw;
 
+	//引数のあるコンストラクタ
+	public LoginBeans(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+
+	//引数のないコンストラクタ
+	public LoginBeans() {
+		this.id = "";
+		this.pw = "";
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
 }
