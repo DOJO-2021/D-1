14e91e5f2823fea4bc8a4d7b37c12ec3edb538d9
dojo2021-ri-hiraoka <hiraoka@seplus2016.onmicrosoft.com>
2021-06-11 12:09:57 +0900

    変更

--- SEEGGS/src/servlet/LoginServlet.java
+++ SEEGGS/src/servlet/LoginServlet.java
@@ -1,41 +1,61 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDAO;
+import model.LoginBeans;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class LoginServlet1
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("ID");
+		String pw = request.getParameter("PW");
 
+		// ログイン処理を行う
+		LoginDAO iDao = new LoginDAO();
+		if (iDao.isLoginOK(id, pw)) {	// ログイン成功
+			// セッションスコープにIDを格納する(変更有)
+			HttpSession session = request.getSession();
+			session.setAttribute("id,pw", new LoginBeans(id,pw));
+
+			// ホームサーブレットにリダイレクトする(変更有)
+			response.sendRedirect("/SEEGGS/HomeServlet");
+		}
+		else {									// ログイン失敗
+			// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+			request.setAttribute("result",
+			new Result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/SEEGGS/LoginServlet"));
+
+			// 結果ページにフォワードする(変更有)
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
 }
