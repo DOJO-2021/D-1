758c0cb8baac337094024afa206abe39a8904743
dojo2021-majima <mazima@seplus2016.onmicrosoft.com>
2021-06-11 14:54:16 +0900

    userupdatedeleteservlet名称変更

--- SEEGGS/src/servlet/FavorServlet.java
+++ SEEGGS/src/servlet/FavorServlet.java
@@ -1,19 +1,24 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.FavorDAO;
 
 /**
- * Servlet implementation class FavorServlet
+ * Servlet implementation class UserServlet
  */
 @WebServlet("/FavorServlet")
 public class FavorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +31,45 @@ public class FavorServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+		// お気に入りページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Favor.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/SEEGGS/LoginServlet");
+				return;
+			}
+
+			// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String u_number = request.getParameter("U_NUMBER");
+			String m_number = request.getParameter("M_NUMBER");
+
+			// 検索処理を行う
+			FavorDAO fDao = new FavorDAO();
+			List<Favorite> cardList = fDao.select(new Favorite(0, 0));
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("favorList", favorList);
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Favor.jsp");
+			dispatcher.forward(request, response);
 	}
 
+
 }
+
---
+++ SEEGGS/src/servlet/UserUpdateDeleteServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDAO;
+
+/**
+ * Servlet implementation class UpdateDeleteServlet
+ */
+@WebServlet("/UpdateDeleteServlet")
+public class UpdateDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int u_number = Integer.parseInt(request.getParameter("U_NUMBER"));
+		String photo = request.getParameter("PHOTO");
+		String name = request.getParameter("NAME");
+		String company = request.getParameter("COMPANY");
+		String nickname = request.getParameter("NICKNAME");
+		String birthplace = request.getParameter("BIRTHPLACE");
+		String thisisme = request.getParameter("THISISME");
+		String hobby = request.getParameter("HOBBY");
+		String future = request.getParameter("FUTURE");
+		String word = request.getParameter("WORD");
+
+		// 更新または削除を行う
+		UserDAO uDao = new UserDAO();
+		if (request.getParameter("SUBMIT").equals("更新")) {
+			if (uDao.update(new User(u_number, name, company, nickname, birthplace, thisisme, hobby, word))) {	// 更新成功
+				request.setAttribute("result",
+				new Result("更新成功！", "データを更新しました。", "/SEEGGS/AHomeServlet"));
+			}
+			else {												// 更新失敗
+				request.setAttribute("result",
+				new Result("更新失敗！", "データを更新できませんでした。", "/SEEGGS/AHomeServlet"));
+			}
+		}
+		else {
+			if (uDao.delete(u_number)) {	// 削除成功
+				request.setAttribute("result",
+				new Result("削除成功！", "ユーザーを削除しました。", "/SEEGGS/AHomeServlet"));
+			}
+			else {						// 削除失敗
+				request.setAttribute("result",
+				new Result("削除失敗！", "ユーザーを削除できませんでした。", "/SEEGGS/AHomeServlet"));
+			}
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AResult.jsp");
+		dispatcher.forward(request, response);
+	}
+}
+
