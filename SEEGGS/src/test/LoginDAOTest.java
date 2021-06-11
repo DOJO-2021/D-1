780fd6baf28b923bd7e62088bb3135ae86f3206b
dojo2021-k-toyota <toyota@seplus2016.onmicrosoft.com>
2021-06-11 15:26:35 +0900

    テストdaoです

---
+++ SEEGGS/src/test/LoginDAOTest.java
@@ -0,0 +1,31 @@
+package test;
+import dao.LoginDAO;
+
+public class LoginDAOTest {
+	public static void main(String[] args) {
+		testIsLoginOK1();	// ユーザーが見つかる場合のテスト
+		testIsLoginOK2();	// ユーザーが見つからない場合のテスト
+	}
+
+	// ユーザーが見つかる場合のテスト
+	public static void testIsLoginOK1() {
+		LoginDAO dao = new LoginDAO();
+		if (dao.isLoginOK("dojo", "pass")) {
+			System.out.println("testIsLoginOK1：テストが成功しました");
+		}
+		else {
+			System.out.println("testIsLoginOK1：テストが失敗しました");
+		}
+	}
+
+	// ユーザーが見つからない場合のテスト
+	public static void testIsLoginOK2() {
+		LoginDAO dao = new LoginDAO();
+		if (!dao.isLoginOK("dojo", "password")) {
+			System.out.println("testIsLoginOK2：テストが成功しました");
+		}
+		else {
+			System.out.println("testIsLoginOK2：テストが失敗しました");
+		}
+	}
+}
---
+++ SEEGGS/src/test/UserDAOTest.java
@@ -0,0 +1,50 @@
+package test;
+import java.util.List;
+
+import dao.UserDAO;
+import model.UserBeans;
+public class UserDAOTest {
+	public static void main(String[] args) {
+		UserDAO dao = new UserDAO();
+
+		// select()のテスト
+		System.out.println("---------- select()のテスト ----------");
+		List<UserBeans> userList = dao.select(new UserBeans(1, "", "", "", "", "", "", "", "", ""));
+		for (UserBeans card : userList) {
+			System.out.println(card.getU_number());
+			System.out.println(card.getPhoto());
+			System.out.println(card.getName());
+			System.out.println(card.getCompany());
+			System.out.println(card.getNickname());
+			System.out.println(card.getBirthplace());
+			System.out.println(card.getThisisme());
+			System.out.println(card.getHobby());
+			System.out.println(card.getFuture());
+			System.out.println(card.getWord());
+			System.out.println();
+			System.out.println();
+		}
+
+		// insert()のテスト
+		System.out.println("---------- insert()のテスト ----------");
+		UserBeans insRec = new UserBeans(0, "TEST", "TEST", "TEST","TEST", "TEST", "一", "TEST", "TEST", "TEST");
+		if (dao.insert(insRec)) {
+			System.out.println("登録成功！");
+		}
+		else {
+			System.out.println("登録失敗！");
+		}
+
+		//挿入したレコードIDを取得する
+		int insU_Number = dao.select(insRec).get(0).getU_number();
+		// update()のテスト
+		System.out.println("---------- update()のテスト ----------");
+		UserBeans upRec = new UserBeans(insU_Number, "update.png", "更新太郎", "更新社", "コウシン君", "トランプタワー", "更", "更新", "全世界を更新するよ", "更新は任せてくれ");
+		if (dao.update(upRec)) {
+			System.out.println("更新成功！");
+		}
+		else {
+			System.out.println("更新失敗！");
+		}
+	}
+}
