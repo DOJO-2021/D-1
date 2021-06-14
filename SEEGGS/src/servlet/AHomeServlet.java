97d1bb575444a7565608623992dbbd8a259472f1
dojo2021-S-yamada <yamada@seplus2016.onmicrosoft.com>
2021-06-14 16:55:25 +0900

    AHomeServlet.java更新

--- SEEGGS/src/servlet/AHomeServlet.java
+++ SEEGGS/src/servlet/AHomeServlet.java
@@ -1,41 +1,44 @@
 package servlet;
-
+?
 import java.io.IOException;
+?
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+?
 /**
- * Servlet implementation class AHomeServlet
+ * Servlet implementation class MenuServlet
  */
-@WebServlet("/AHomeServlet")
+@WebServlet("/AHomeServlet")//変更有
 public class AHomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AHomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+?
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+?
+		//セッションスコープにインスタンスを保存
+				session.setAttribute("id", id);
+?
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String u_number = request.getParameter("U_NUMBER");
+		String id = request.getParameter("ID");
+		String password = request.getParameter("PASSWORD");
+?
+		//AHomeページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AHome.jsp");
+		dispatcher.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
