ce86301df33b0b49dbe533e97b7acedb66e7164b
dojo2021-ri-hiraoka <hiraoka@seplus2016.onmicrosoft.com>
2021-06-16 16:41:52 +0900

    確認

--- SEEGGS/src/servlet/ThreadUpdateDeleteServlet.java
+++ SEEGGS/src/servlet/ThreadUpdateDeleteServlet.java
@@ -1,7 +1,7 @@
 package servlet;
-?
+
 import java.io.IOException;
-?
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,17 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-?
+
 import dao.BoardUpdateDeleteDAO;
 import model.ResultBeans;
-?
+
 /**
  * Servlet implementation class UpdateDeleteServlet
  */
 @WebServlet("/ThreadUpdateDeleteServlet")
 public class ThreadUpdateDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-?
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -30,13 +30,13 @@ public class ThreadUpdateDeleteServlet extends HttpServlet {
 			response.sendRedirect("/SEEGGS/LoginServlet");
 			return;
 		}
-?
+
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		int m_number = Integer.parseInt(request.getParameter("M_NUMBER"));
 		int type = Integer.parseInt(request.getParameter("TYPE"));
 		String coments = request.getParameter("COMENTS");
-?
+
 		// 更新または削除を行う
 		BoardUpdateDeleteDAO budDao = new BoardUpdateDeleteDAO();
 		if (request.getParameter("SUBMIT").equals("更新")) {
@@ -59,7 +59,7 @@ public class ThreadUpdateDeleteServlet extends HttpServlet {
 				new ResultBeans("ユーザーを削除できませんでした。", "/SEEGGS/AHomeServlet"));
 			}
 		}
-?
+
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AResult.jsp");
 		dispatcher.forward(request, response);
