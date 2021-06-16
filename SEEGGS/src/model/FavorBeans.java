8e001549add031d84a7d129cf6648f650bb16326
dojo2021-k-toyota <toyota@seplus2016.onmicrosoft.com>
2021-06-16 16:25:14 +0900

    6/16model.test

--- SEEGGS/src/model/FavorBeans.java
+++ SEEGGS/src/model/FavorBeans.java
@@ -1,5 +1,50 @@
 package model;
+import java.io.Serializable;
 
-public class FavorBeans {
+public class FavorBeans implements Serializable{
+//フィールドの作成
+private String id;
+private int m_number;
+private String contents;
+
+//引数のあるコンストラクタ
+public FavorBeans(String id, int m_number, String contents) {
+	super();
+	this.id= id;
+	this.m_number= m_number;
+	this.contents=contents;
+}
+
+//引数のないコンストラクタ
+public FavorBeans() {
+	super();
+	this.id="";
+	this.m_number=0;
+	this.contents="";
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+public int getM_number() {
+	return m_number;
+}
+
+public void setM_number(int m_number) {
+	this.m_number = m_number;
+}
+
+public String getContents() {
+	return contents;
+}
+
+public void setContents(String contents) {
+	this.contents = contents;
+}
 
 }
