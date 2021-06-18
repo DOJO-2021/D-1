7e841bb9a0387002b9308330195558acc37a46a9
dojo2021-umino <85271562+dojo2021-umino@users.noreply.github.com>
2021-06-18 13:38:02 +0900

    BoardServlet/ThreadServletの更新

--- SEEGGS/src/servlet/ThreadServlet.java
+++ SEEGGS/src/servlet/ThreadServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.BoardDAO;
+import model.BoardBeans;
 
 /**
  * Servlet implementation class ThreadServlet
@@ -13,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ThreadServlet")
 public class ThreadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +33,104 @@ public class ThreadServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/LoginServlet");
+			return;
+		}
+		// スレッドページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/LoginServlet");
+			return;
+		}
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int m_number = Integer.parseInt(request.getParameter("m_number"));
+		int type = Integer.parseInt(request.getParameter("type"));
+		String contents = request.getParameter("contents");
+
+		// 検索処理を行う
+		BoardDAO bDao = new BoardDAO();
+		List<BoardBeans> BoardList = bDao.select(new BoardBeans(m_number,type,contents));
+
+
+		//掲示板の種類ごとに検索結果をスレッドに表示する。
+		if (request.getParameter("SUBMIT").equals("ノウハウ共有掲示板")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		if (request.getParameter("SUBMIT").equals("エラー共有掲示板")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		if (request.getParameter("SUBMIT").equals("お悩み相談掲示板")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		if (request.getParameter("SUBMIT").equals("講師に質問！")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		if (request.getParameter("SUBMIT").equals("ジモトーーーク！")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		if (request.getParameter("SUBMIT").equals("休日何してる？")) {
+
+
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("BoardList", BoardList);
+
+			// 掲示板ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Thread.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
