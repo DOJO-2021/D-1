c3390b8e2fa1a33d629a1f17b84cbc14558bf79c
dojo2021-sasaki-m <85271522+dojo2021-sasaki-m@users.noreply.github.com>
2021-06-18 15:10:30 +0900

    AccountServlet修正

--- SEEGGS/src/servlet/AccountServlet.java
+++ SEEGGS/src/servlet/AccountServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 import dao.UserDAO;
@@ -37,12 +36,14 @@ public class AccountServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
 			response.sendRedirect("LoginServlet");
 			return;
 		}
+		*/
 
 		// 登録ページにフォワードする
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Account.jsp");
@@ -53,13 +54,13 @@ public class AccountServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+	/*	// もしもログインしていなかったらログインサーブレットにリダイレクトする
 				HttpSession session = request.getSession();
 				if (session.getAttribute("id") == null) {
 					response.sendRedirect("LoginServlet");
 					return;
 				}
-
+*/
 				// リクエストパラメータを取得する
 				request.setCharacterEncoding("UTF-8");
 
