fea16cd773aa57b67c5d931797031d9314cdae07
dojo2021-umino <85271562+dojo2021-umino@users.noreply.github.com>
2021-06-18 13:57:14 +0900

    ResultServlet更新

--- SEEGGS/src/servlet/ResultServlet.java
+++ SEEGGS/src/servlet/ResultServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.BoardDAO;
+import model.BoardBeans;
 
 /**
  * Servlet implementation class ResultServlet
@@ -13,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ResultServlet")
 public class ResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +33,44 @@ public class ResultServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+					HttpSession session = request.getSession();
+					if (session.getAttribute("id") == null) {
+						response.sendRedirect("/SEEGGS/LoginServlet");
+						return;
+					}
+					// リクエストパラメータを取得する
+					request.setCharacterEncoding("UTF-8");
+					int m_number = Integer.parseInt(request.getParameter("m_number"));
+					int type = Integer.parseInt(request.getParameter("type"));
+					String contents = request.getParameter("contents");
+
+					// 検索処理を行う
+					BoardDAO bDao = new BoardDAO();
+					List<BoardBeans> BoardList = bDao.select(new BoardBeans(m_number,type,contents));
+
+					// 検索結果をリクエストスコープに格納する
+					request.setAttribute("BoardList", BoardList);
+
+					RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/Result.jsp");
+					d.forward(request, response);
+					return;
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/SEEGGS/LoginServlet");
+			return;
+		}
+
+		// 掲示板ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Result.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
