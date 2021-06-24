e50b2eae08fbe5773b9827462c531deaa95efc7b
dojo2021-s-yamada <yamada@seplus2016.onmicrosoft.com>
2021-06-24 17:10:35 +0900

    サーブレット更新

--- SEEGGS/src/servlet/ProfileServlet.java
+++ SEEGGS/src/servlet/ProfileServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.UserDAO;
-import model.ResultBeans;
 import model.UserBeans;
 
 /**
@@ -47,8 +47,12 @@ public class ProfileServlet extends HttpServlet {
 		return;
 	}
 		// リクエストパラメータを取得する
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
+	request.setCharacterEncoding("UTF-8");
+		ArrayList<UserBeans> userList =(ArrayList)session.getAttribute("User");
+		UserBeans user = userList.get(0);
+		String id = user.getId();
+		String password = user.getPassword();
+
 		String photo = request.getParameter("photo");
 		String name = request.getParameter("name");
 		String company = request.getParameter("company");
@@ -58,17 +62,19 @@ public class ProfileServlet extends HttpServlet {
 		String hobby = request.getParameter("hobby");
 		String future = request.getParameter("future");
 		String word = request.getParameter("word");
+		user = new UserBeans(id, password, photo, name, company, nickname, birthplace, thisisme, hobby, future, word);
 
 		// 更新を行う
 		UserDAO uDao = new UserDAO();
 		if (request.getParameter("SUBMIT").equals("更新")) {
-			if (uDao.update(new UserBeans(id, password, photo, name, company, nickname, birthplace, thisisme, hobby, future, word))) {	// 更新成功
-				request.setAttribute("result",
-				new ResultBeans("データを更新しました。", "HomeServlet"));
-			}
-			else {												// 更新失敗
-				request.setAttribute("result",
-				new ResultBeans("データを更新できませんでした。", "HomeServlet"));
+			if (uDao.update(user)) {	// 更新成功
+				session.setAttribute("User",  user);
+				//				request.setAttribute("result",
+//				new ResultBeans("データを更新しました。", "HomeServlet"));
+//			}
+//			else {												// 更新失敗
+//				request.setAttribute("result",
+//				new ResultBeans("データを更新できませんでした。", "HomeServlet"));
 			}
 		}
 
